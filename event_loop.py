176f0e437f5af0527469796d9bf3fb76046d0e1a
Tomas Pulai <tomas.pulai@fiserv.com>
2024-03-17 10:36:26 +0100

    remove not needed part form event_loop.py

--- event_loop.py
+++ event_loop.py
@@ -23,13 +23,6 @@ def hog():
     return sum
 
 
-async def keep_printing(name=""):
-    while True:
-        print(name, end=" ")
-        print_now()
-        await asyncio.sleep(0.5)
-
-
 
 loop = asyncio.get_event_loop()
 
@@ -37,8 +30,8 @@ loop.call_soon(trampoline, "One")
 loop.call_soon(trampoline, "Two")
 loop.call_soon(trampoline, "Three")
 
-loop.call_later(5, hog)
+# loop.call_later(5, hog)
 
-loop.call_later(15, loop.stop)
+loop.call_later(20, loop.stop)
 
 loop.run_forever()
